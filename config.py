8fa38a4e623263c665d98a523571c741c22a87dd
divyanshprakash <74813755+divyanshprakash@users.noreply.github.com>
2021-08-11 00:33:33 +0530

    Update

--- config.py
+++ config.py
@@ -1,24 +1,4 @@
-#MIT License
 
-#Copyright (c) 2021 SUBIN
-
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
-
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
 import os
 import re
 from youtube_dl import YoutubeDL
@@ -44,16 +24,16 @@ else:
 class Config:
     ADMIN = os.environ.get("ADMINS", '')
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
-    API_ID = int(os.environ.get("API_ID", ''))
-    CHAT = int(os.environ.get("CHAT", ""))
-    LOG_GROUP=os.environ.get("LOG_GROUP", "")
+    API_ID = int(os.environ.get("API_ID", '6239579'))
+    CHAT = int(os.environ.get("CHAT", "-1001172836875"))
+    LOG_GROUP=os.environ.get("LOG_GROUP", "-1001172836875")
     if LOG_GROUP:
         LOG_GROUP=int(LOG_GROUP)
     else:
         LOG_GROUP=None
-    STREAM_URL=finalurl
+    
     ADMIN_ONLY=os.environ.get("ADMIN_ONLY", "N")
-    ARQ_API=os.environ.get("ARQ_API", "")
+    ARQ_API=os.environ.get("ARQ_API", "DXBOAO-MVWXVU-OIUMQY-ZVTTIH-ARQ")
     REPLY_MESSAGE=os.environ.get("REPLY_MESSAGE", None)
     if REPLY_MESSAGE:
         REPLY_MESSAGE=REPLY_MESSAGE
@@ -64,9 +44,8 @@ class Config:
         EDIT_TITLE=None
     DURATION_LIMIT=int(os.environ.get("MAXIMUM_DURATION", 15))
     DELAY = int(os.environ.get("DELAY", 10))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    SESSION = os.environ.get("SESSION_STRING", "")
+    API_HASH = os.environ.get("API_HASH", "0a6bb5be3bec0cb8ed7c408a45f02f4c")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1938493507:AAH6-PLMIfHDIe0sTF1XkPw_J0IZ-dyFLjM") 
+    SESSION = os.environ.get("SESSION_STRING", "BQAvVIPjC4GsmJ6oBmvNYRd1ITtF0zBKxxOTVGjABJY2B-htpwK1L5JUu4gnZMgxW1lLYs-CyhnpxNNKTZdYOKsi5i2zMrGCLKCwczHXj_lTFvgub3KZWPhlevcSnEd0Kqdt9rQPUuZvsPEvrxott4SBDtjdF3a8B6nclYwMfy-SO2_ECP3Y6Sy5rbh2Ked7afnk7Zw_zHaolTQq8V6xmmOHNUU9lHcGwMdN6ZulundYLDzhxptUYdwVH7qCqXd4SUM57sybVmx6Kda-yzE3wTSlz6JhdeAx5zfrG6PCN45eQBUrD94DC7AgiqKGkrX8a9oQ5Oky0_98UOIkTuno5m8TYaBlPwA")
     playlist=[]
     msg = {}
-
