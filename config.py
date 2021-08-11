cce0321d5510cdd637f069960fadecdda1c59bc4
divyanshprakash <74813755+divyanshprakash@users.noreply.github.com>
2021-08-11 22:34:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,18 +42,18 @@ else:
     finalurl=STREAM
 
 class Config:
-    ADMIN = os.environ.get("ADMINS", '')
+    ADMIN = os.environ.get("ADMINS", '691224603')
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
-    API_ID = int(os.environ.get("API_ID", ''))
-    CHAT = int(os.environ.get("CHAT", ""))
-    LOG_GROUP=os.environ.get("LOG_GROUP", "")
+    API_ID = int(os.environ.get("API_ID", '6239579'))
+    CHAT = int(os.environ.get("CHAT", "-1001591741014"))
+    LOG_GROUP=os.environ.get("LOG_GROUP", "-1001591741014")
     if LOG_GROUP:
         LOG_GROUP=int(LOG_GROUP)
     else:
         LOG_GROUP=None
     STREAM_URL=finalurl
     ADMIN_ONLY=os.environ.get("ADMIN_ONLY", "N")
-    ARQ_API=os.environ.get("ARQ_API", "")
+    ARQ_API=os.environ.get("ARQ_API", "DXBOAO-MVWXVU-OIUMQY-ZVTTIH-ARQ")
     REPLY_MESSAGE=os.environ.get("REPLY_MESSAGE", None)
     if REPLY_MESSAGE:
         REPLY_MESSAGE=REPLY_MESSAGE
@@ -62,10 +62,10 @@ class Config:
     EDIT_TITLE = os.environ.get("EDIT_TITLE", True)
     if EDIT_TITLE == "NO":
         EDIT_TITLE=None
-    DURATION_LIMIT=int(os.environ.get("MAXIMUM_DURATION", 15))
+    DURATION_LIMIT=int(os.environ.get("MAXIMUM_DURATION", 1500))
     DELAY = int(os.environ.get("DELAY", 10))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    SESSION = os.environ.get("SESSION_STRING", "")
+    API_HASH = os.environ.get("API_HASH", "0a6bb5be3bec0cb8ed7c408a45f02f4c")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1938493507:AAGp7loeG0xbeUMgEUUZW7btluegC1QI8zs") 
+    SESSION = os.environ.get("SESSION_STRING", "BQCGtpP6rvtC0jnZa4YNWEV5DAnfk-uZi1qNyFqBoeLsbapH-G_LJVwCQeu2ouYljGyfZ16vmZD-X-3L3VJ6yeFLEOzIVJHzYXz0IMk2GS3ADFXYykrXTiRGZC_OWovj8j7UyHdLFig22QZMpkX4YyrvOpsSgLvJV5HKrmgFgFNQVPyvYC-bn8nl3XLykuBfXf5wLtFokNQon_WNVKeCD_pyxcF9SWyuN2QR3AgRUL_Mjpnv9UnM4rJjEdKcuoPMeKL5Sv56qiJ7kU0Jmk8ZbTC8iaSM8KF2313Q8BmdPiVN9CE0nCkIaTplmDSl8uJExwexvWqSHqKGE7vdDosd5Y5oYaBlPwA")
     playlist=[]
     msg = {}
